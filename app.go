ec36d8a478a417891d2c91a4829e2607fdce31f9
ros-tel <vladvladow@yandex.ru>
2018-03-23 17:40:45 +0500

    Fixed a bug with the use of TLS. Check all IPs for a domain.

--- app.go
+++ app.go
@@ -3,10 +3,20 @@ package main
 import (
 	"flag"
 	"log"
+	"strconv"
 	"strings"
 )
 
+type (
+	host struct {
+		host string
+		port string
+	}
+)
+
 var (
+	hosts []host
+
 	param_hosts = flag.String("hosts", "", "Usage: -hosts=google.com:80 or -hosts=87.250.250.242:80,google.com:80")
 	use_tls     = flag.Bool("tls", false, "Usage: -tls")
 )
@@ -21,7 +31,28 @@ func main() {
 		log.Fatal("Required parameter \"hosts\" not set")
 	}
 
-	hosts := strings.Split(*param_hosts, ",")
+	ph := strings.Split(*param_hosts, ",")
+	for _, hv := range ph {
+		h := strings.Split(hv, ":")
+
+		// No port specified. Use 80 as the default port
+		if len(h) == 1 {
+			hosts = append(hosts, host{host: h[0], port: "80"})
+			continue
+		}
+
+		port, err := strconv.Atoi(h[1])
+		if err != nil || port < 1 || 65535 < port {
+			log.Printf("Bad port %s\n", hv)
+			continue
+		}
+
+		hosts = append(hosts, host{host: h[0], port: h[1]})
+	}
+
+	if len(hosts) == 0 {
+		log.Fatal("No host is specified for testing")
+	}
 
 	checkerRun(hosts)
 }
--- checker.go
+++ checker.go
@@ -4,39 +4,39 @@ import (
 	"crypto/tls"
 	"log"
 	"net"
-	"strings"
 	"time"
 )
 
-func checkerRun(hosts []string) {
+func checkerRun(hosts []host) {
 	for {
 		var conn net.Conn
 		var err error
 		var ips []string
 
 		for _, v := range hosts {
-			h := strings.Split(v, ":")
-			ips, err = net.LookupHost(h[0])
+			ips, err = net.LookupHost(v.host)
 			if err != nil {
-				log.Printf("[ERROR] Lookup Host %s error: %v\n", h[0], err)
+				log.Printf("[ERROR] Lookup Host %s error: %v\n", v.host, err)
 				continue
 			}
-			host := ips[0] + ":" + h[1]
+			for _, ip := range ips {
+				host := ip + ":" + v.port
 
-			if *use_tls {
-				dialer := &net.Dialer{Timeout: 5 * time.Second}
-				conn, err = tls.DialWithDialer(dialer, "tcp", host, &tls.Config{InsecureSkipVerify: false})
-			} else {
-				conn, err = net.DialTimeout("tcp", host, 5*time.Second)
+				if *use_tls {
+					dialer := &net.Dialer{Timeout: 5 * time.Second}
+					conn, err = tls.DialWithDialer(dialer, "tcp", host, &tls.Config{InsecureSkipVerify: false, ServerName: v.host})
+				} else {
+					conn, err = net.DialTimeout("tcp", host, 5*time.Second)
+				}
+				if err != nil {
+					log.Printf("[ERROR] dial to %s:%s usage tls %t failed error: %v\n", v.host, v.port, *use_tls, err)
+					continue
+				}
+
+				conn.Close()
+
+				log.Printf("[OK] dial to %s:%s usage tls %t remote addr %s\n", v.host, v.port, *use_tls, conn.RemoteAddr())
 			}
-			if err != nil {
-				log.Printf("[ERROR] dial to %s usage tls %t failed error: %v\n", v, *use_tls, err)
-				continue
-			}
-
-			conn.Close()
-
-			log.Printf("[OK] dial to %s usage tls %t remote addr %s\n", v, *use_tls, conn.RemoteAddr())
 		}
 
 		time.Sleep(10 * time.Second)
